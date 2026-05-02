fbb9fb828a0f50ac9dccd13fb33600a89c927eea
48774861 <112006435+48774861@users.noreply.github.com>
2026-05-02 09:29:22 -0500

    Deleted Files that were useless

--- options.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("backBtn");
-
-  btn.addEventListener("click", () => {
-    const url = chrome.runtime.getURL("newtab.html");
-    window.location.href = url;
-  });
-});
